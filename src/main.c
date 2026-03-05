7cf8572c629826dc2c8be8654e14c570f3a65830
Frédéric Brière <fbriere@fbriere.net>
2026-03-05 12:53:02 -0500

    Fix crash when `IDLE_XVPIC_NUM_PER_CALL` is greater than 1

--- src/main.c
+++ src/main.c
@@ -2702,6 +2702,7 @@ for(f=0;f<IDLE_XVPIC_NUM_PER_CALL;f++)
     /* can't have jump to return from, so just remove ourselves. */
     stop_thumbnail_read();
     *entryp=-1;
+    break;
     }
   }
 return 1;
