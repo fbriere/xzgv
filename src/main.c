67a41687c29f04958b55d5b196bc4e539cdaf0f4
Frédéric Brière <fbriere@fbriere.net>
2026-03-27 17:10:44 -0400

    gtk3: Rename `clist_add_new_row()`

--- src/main.c
+++ src/main.c
@@ -2833,7 +2833,7 @@ return(1);			/* else second one is dir */
 }
 
 
-int clist_add_new_row(char *filename,struct stat *sbuf)
+int add_new_row(char *filename,struct stat *sbuf)
 {
 struct row_data_tag *datptr;
 gchar *textarr[SELECTOR_NUM_COLUMNS];
@@ -2975,7 +2975,7 @@ while((dent=readdir(dirfile))!=NULL)
     sbuf.st_atime=0;
     }
   
-  if(clist_add_new_row(dent->d_name,&sbuf))
+  if(add_new_row(dent->d_name,&sbuf))
     numrows++;
   }
 
@@ -4232,7 +4232,7 @@ for(f=argc-argsleft;f<=argc-1;f++)
   /* can't use isdir() as that has different reaction to stat() failing */
   if(stat(argv[f],&sbuf)!=-1 && !S_ISDIR(sbuf.st_mode))
     {
-    if(clist_add_new_row(argv[f],&sbuf))
+    if(add_new_row(argv[f],&sbuf))
       numrows++;
     }
   }
