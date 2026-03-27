b2c21f3c1651d7b52f229235b3b85abb2a21ba33
Frédéric Brière <fbriere@fbriere.net>
2026-03-27 10:02:41 -0400

    gtk3: Add wrappers for `gtk_clist_get_selection_info()`
    Porting to GTK 3 will involve migrating from `GtkCList` to
    `GtkTreeView`.  Many `gtk_clist_*` functions do not have a single
    `gtk_tree_view_*` equivalent; to make the transition smoother, we write
    wrappers around these, so that we will only need to modify the wrapper's
    definition, instead of every single invocation.
    
    Despite the name, `gtk_clist_get_selection_info()` has nothing to do
    with selections; rather, it returns the row and column at a specified
    pixel position.
    
    We actually use this function for two very different purposes:
    
      - To obtain the first visible row, passing the coordinates (0,0)
      - To obtain the row matching the coordinates of a button event
    
    These will have to be handled differently in GTK 3, hence the need for
    two distinct handlers.

--- src/main.c
+++ src/main.c
@@ -389,6 +389,17 @@ return(TRUE);
 }
 
 
+int get_row_at_pos(int x, int y)
+{
+  int row, col;
+
+  if (!gtk_clist_get_selection_info(GTK_CLIST(clist), x, y, &row, &col))
+    return -1;
+
+  return row;
+}
+
+
 /* NOTE: The caller takes ownership ot *filename, and is responsible for freeing it. */
 void get_row_filename(int row, char **filename)
 {
@@ -446,6 +457,17 @@ void unselect_row(int row)
 void unselect_all(void)
 {
   gtk_clist_unselect_all(GTK_CLIST(clist));
+}
+
+
+int first_visible_row(void)
+{
+  const int x = 0, y = 0;  /* point (0,0) at the lop-left */
+  int row, col;
+
+  gtk_clist_get_selection_info(GTK_CLIST(clist), x, y, &row, &col);
+
+  return row;
 }
 
 
@@ -659,7 +681,7 @@ XIconifyWindow(GDK_WINDOW_XDISPLAY(main_gdk_window),
 
 gint selector_button_press(GtkWidget *widget,GdkEventButton *event)
 {
-int row,col;
+int row;
 
 if(ignore_selector_input)
   {
@@ -690,8 +712,7 @@ switch(event->button)
   
   case 3:
     /* move cursor to row clicked on (if any) */
-    gtk_clist_get_selection_info(GTK_CLIST(clist),
-                                 event->x,event->y,&row,&col);
+    row = get_row_at_pos(event->x, event->y);
     cb_back_to_clist();			/* show selector and switch to it */
     if(row>=0 && row<numrows)
       set_focus_row(row);
@@ -707,7 +728,7 @@ return(FALSE);
 
 gint selector_button_release(GtkWidget *widget,GdkEventButton *event)
 {
-int row,col;
+int row;
 
 if(ignore_selector_input)
   {
@@ -720,8 +741,7 @@ switch(event->button)
   case 1:
     if(event->state&GDK_CONTROL_MASK)
       {
-      gtk_clist_get_selection_info(GTK_CLIST(clist),
-                                   event->x,event->y,&row,&col);
+      row = get_row_at_pos(event->x, event->y);
       if(row>=0 && row<numrows)		/* sanity check :-) */
         set_tagged_state(row,-1);	/* toggle */
       return(TRUE);
@@ -2250,11 +2270,11 @@ if(thumbnail_read_running())
 void blocking_thumbnail_read_visible(GtkWidget **checkptr)
 {
 int entry;
-int row=-1,col=-1;
+int row=-1;
 
 if(thumbnail_read_running()) return;
 
-gtk_clist_get_selection_info(GTK_CLIST(clist),0,0,&row,&col);
+row = first_visible_row();
 if(row==-1) return;
 
 idle_xvpic_lastadjval=gtk_adjustment_get_value(gtk_clist_get_vadjustment(GTK_CLIST(clist)));
@@ -2682,7 +2702,7 @@ if(idle_xvpic_blocked)
 adjval=gtk_adjustment_get_value(gtk_clist_get_vadjustment(GTK_CLIST(clist)));
 if(adjval!=idle_xvpic_lastadjval)
   {
-  int row=-1,col=-1;
+  int row=-1;
   
   idle_xvpic_lastadjval=adjval;
   
@@ -2692,7 +2712,7 @@ if(adjval!=idle_xvpic_lastadjval)
    * big dirs, even though in practice if you move about a lot
    * it can actually increase it somewhat. :-)
    */
-  gtk_clist_get_selection_info(GTK_CLIST(clist),0,0,&row,&col);
+  row = first_visible_row();
   if(row!=-1)
     {
     idle_xvpic_jumped++;
