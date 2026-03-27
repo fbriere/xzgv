c11f9ce67bd67b01c0361098805396b40a8883c2
Frédéric Brière <fbriere@fbriere.net>
2026-03-27 06:33:29 -0400

    gtk3: Add wrappers for `gtk_clist_get/set_pixmap()`
    Porting to GTK 3 will involve migrating from `GtkCList` to
    `GtkListStore`.  Many `gtk_clist_*` functions do not have a single
    `gtk_list_store_*` equivalent; to make the transition smoother, we write
    wrappers around these, so that we will only need to modify the wrapper's
    definition, instead of every single invocation.

--- src/main.h
+++ src/main.h
@@ -38,6 +38,8 @@ extern xzgv_image *load_image(char *file,int for_thumbnail,
                                  int *origwp,int *orighp);
 extern void get_row_filename(int row, char **text);
 extern void set_row_filename(int row, char *text);
+extern int get_row_thumbnail(int row, GdkPixmap **pixmap, GdkBitmap **mask);
+extern void set_row_thumbnail(int row, GdkPixmap *pixmap, GdkBitmap *mask);
 extern void make_visible_if_not(int row);
 extern int get_tagged_state(int row);
 extern int thumbnail_read_running(void);
