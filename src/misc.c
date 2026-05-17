fbriere/xzgv

commit 9ffb9ffb1fe2d1811502b1e85177d93247e2edfc

Remember our own current path location, before symlink resolution
Navigating across symlinked directories can be somewhat confusing with
xzgv, as `chdir()` will resolve the link when going down such a
directory, so that going back up via the parent may not return to the
original location.

File browsers will typically maintain their own internal path location,
removing the last element when asked to go up to the parent directory.
This is what we're doing here, replacing `chdir()` and `getcwd()` with
wrappers of our own.

src/misc.c
@@ -12,7 +12,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <glib.h>
+
 #include "misc.h"
+
+
+static gchar *current_dir;
 
 
 /* a crude quit for when malloc and the like fails */
@@ -20,6 +25,40 @@ void quit_no_mem(void)
 {
 fprintf(stderr,"xzgv: out of memory\n");
 exit(1);
+}
+
+
+void init_current_dir(void)
+{
+if (!current_dir)
+  current_dir = g_get_current_dir();
+}
+
+
+char *xzgv_getcwd(char *buf,size_t size)
+{
+init_current_dir();
+if (g_strlcpy(buf,current_dir,size) >= size)
+  return NULL;
+else
+  return buf;
+}
+
+
+int xzgv_chdir(const char *path)
+{
+int retval;
+
+init_current_dir();
+gchar *target_dir = g_canonicalize_filename(path,current_dir);
+retval = chdir(target_dir);
+
+if (retval == 0) {
+  g_free(current_dir);
+  current_dir = target_dir;
+}
+
+return retval;
 }
 
 
@@ -32,7 +71,7 @@ int incr=1024;
 int size=incr;
 char *buf=malloc(size);
 
-while(buf!=NULL && getcwd(buf,size-1)==NULL)
+while(buf!=NULL && xzgv_getcwd(buf,size-1)==NULL)
   {
   free(buf);
   size+=incr;

src/copymove.c
@@ -17,6 +17,7 @@
 #include <gtk/gtk.h>
 #include <gdk/gdkkeysyms.h>
 #include "backend.h"
+#include "misc.h"
 #include "main.h"
 
 #include "copymove.h"
@@ -308,7 +309,7 @@ if(!ok_for_copymove())
 
 cm_do_move=do_move;		/* save for later */
 
-getcwd(cdir,sizeof(cdir)-1);
+xzgv_getcwd(cdir,sizeof(cdir)-1);
 
 dir_win=gtk_dialog_new();
 

src/gotodir.c
@@ -13,6 +13,7 @@
 #include <gtk/gtk.h>
 #include <gdk/gdkkeysyms.h>
 #include "backend.h"
+#include "misc.h"
 #include "main.h"
 
 #include "gotodir.h"
@@ -38,7 +39,7 @@ if(*ptr=='~' && getenv("HOME"))		/* kludge for home dir */
 else
   ptr=g_strdup(ptr);	/* not needed but easier this way :-) */
 
-ret=chdir(ptr);
+ret=xzgv_chdir(ptr);
 
 free((void *)ptr);
 gtk_widget_destroy(dir_win);
@@ -63,7 +64,7 @@ GtkWidget *label,*entry;
 static char cdir[1024],buf[1024];
 int tbl_row;
 
-getcwd(cdir,sizeof(cdir)-1);
+xzgv_getcwd(cdir,sizeof(cdir)-1);
 
 dir_win=gtk_dialog_new();
 

src/main.c
@@ -2918,7 +2918,7 @@ if((dirfile=opendir("."))==NULL)
   /* be sure to mention what we're doing first... :-) */
   if(getenv("HOME")==NULL)
     goto badhome;
-  chdir(getenv("HOME"));
+  xzgv_chdir(getenv("HOME"));
   if((dirfile=opendir("."))==NULL)
     {
     badhome:
@@ -2937,7 +2937,7 @@ if((dirfile=opendir("."))==NULL)
 
 current_selection=-1;
 
-getcwd(cdir,sizeof(cdir)-1);
+xzgv_getcwd(cdir,sizeof(cdir)-1);
 
 /* originally had a `reading directory' msg here, but it's so fast
  * even for big dirs that it hardly seems worth it.
@@ -3002,7 +3002,7 @@ strcpy(buf,"xzgv");
 if(include_dir)
   {
   strcat(buf,": ");
-  getcwd(buf+strlen(buf),sizeof(buf)-strlen(buf)-2);
+  xzgv_getcwd(buf+strlen(buf),sizeof(buf)-strlen(buf)-2);
   }
 
 gtk_window_set_title(GTK_WINDOW(mainwin),buf);
@@ -3387,7 +3387,7 @@ if(datptr->isdir)
   /* if it's a dir, chdir to it and read files there instead. */
   cb_back_to_clist();	/* in case of mouse click, to show pastpos action */
   new_pastpos(row);
-  chdir(ptr);
+  xzgv_chdir(ptr);
   reinit_dir(1,0);	/* reinit and do pastpos */
 
   selector_unblock();
@@ -4160,7 +4160,7 @@ else
 backend_set_interp(interp);
 
 if(argsleft==1 && isdir(argv[optind]))
-  chdir(argv[optind]);	/* change to start directory */
+  xzgv_chdir(argv[optind]);	/* change to start directory */
 else
   {
   if(argsleft>=1)

src/misc.h
@@ -5,4 +5,6 @@
  */
 
 extern void quit_no_mem(void);
+extern int xzgv_chdir(const char *path);
+extern char *xzgv_getcwd(char *buf,size_t size);
 extern char *getcwd_allocated(void);

src/updatetn.c
@@ -386,7 +386,7 @@ if(!mainwin || !*update_tn_win_ptr)
 /* save old cwd to avoid depending on chdir("..") to be sane :-) */
 old_cwd=getcwd_allocated();
 
-if(stat(dirname,&sbuf)==-1 || chdir(dirname)==-1)
+if(stat(dirname,&sbuf)==-1 || xzgv_chdir(dirname)==-1)
   {
   free(old_cwd);
   return;
@@ -398,7 +398,7 @@ if(stat(dirname,&sbuf)==-1 || chdir(dirname)==-1)
 for(f=0;f<num_olddirs;f++)
   if(sbuf.st_dev==olddirs[f].device && sbuf.st_ino==olddirs[f].inode)
     {
-    chdir(old_cwd);
+    xzgv_chdir(old_cwd);
     free(old_cwd);
     return;
     }
@@ -413,7 +413,7 @@ olddirs[ent].inode=sbuf.st_ino;
 /* open the dir */
 if((dir=opendir("."))==NULL)
   {
-  chdir(old_cwd);
+  xzgv_chdir(old_cwd);
   free(old_cwd);
   return;
   }
@@ -448,7 +448,7 @@ if(*update_tn_win_ptr && mainwin)
   }
 
 /* return to previous dir */
-chdir(old_cwd);
+xzgv_chdir(old_cwd);
 free(old_cwd);
 }
 
@@ -496,7 +496,7 @@ olddir_uninit();
 if(!mainwin)
   {
   /* probably unnecessary in practice, but WTF */
-  chdir(origdir);
+  xzgv_chdir(origdir);
   free(origdir);
   return;
   }
@@ -506,7 +506,7 @@ if(update_tn_win)
   gtk_widget_destroy(update_tn_win);
 
 /* return to original dir and rescan it */
-chdir(origdir);
+xzgv_chdir(origdir);
 free(origdir);
 reinit_dir(1,0);	/* init with pastpos */
 

commit 8e9e12a8e2f3928aea6681ce4a24fe766de95c8d

Fix memory leak on `chdir()` failure
(This bug was introduced with commit 9ffb9ff.)

src/misc.c
@@ -56,6 +56,8 @@ retval = chdir(target_dir);
 if (retval == 0) {
   g_free(current_dir);
   current_dir = target_dir;
+} else {
+  g_free(target_dir);
 }
 
 return retval;