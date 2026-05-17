1c464e17b802fc14e4e569e3aafbf095cc48f50d
Frédéric Brière <fbriere@fbriere.net>
2026-05-16 22:01:32 -0400

    Properly show/hide scrollbars when resizing in panorama mode
    When resizing in panorama mode, scrollbars were not properly toggled
    on-the-spot if the scrolling direction changed.  We fix this by also
    setting the scrolling policy in `render_pixmap()`, while taking the
    opportunity to create a single function for this instead of copy-pasting
    more calls to `gtk_scrolled_window_set_policy()`.
    
    This also sets the policy to "never" (instead of "automatic") when in
    non-panorama zoom mode, to prevent some scrollbar flickering that would
    sometimes happen when resizing.
    
    (The calls in `toggle_zoom()` can be removed, as `render_pixmap()` is
    immediately called afterwards.)

--- src/main.c
+++ src/main.c
@@ -1338,6 +1338,30 @@ else
 }
 
 
+void adjust_sw_policy(void)
+{
+  GtkPolicyType policy_h, policy_v;
+
+  if (zoom)
+  {
+    /* disable scrollbars (except for one of them when using panorama)
+     * Note: although AUTOMATIC should work fine in theory, it can result in
+     *       scrollbars popping in and out of existence when resizing
+     * Note: panorama can't use ALWAYS, because of zoom-reduce-only
+     */
+    policy_h = (zoom_panorama && !zoom_panorama_sb) ? GTK_POLICY_AUTOMATIC : GTK_POLICY_NEVER;
+    policy_v = (zoom_panorama && zoom_panorama_sb) ? GTK_POLICY_AUTOMATIC : GTK_POLICY_NEVER;
+  }
+  else
+  {
+    policy_h = GTK_POLICY_AUTOMATIC;
+    policy_v = GTK_POLICY_AUTOMATIC;
+  }
+
+  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw_for_pic), policy_h, policy_v);
+}
+
+
 /* render pixmap from image, resize drawing area to fit, and just
  * generally update things. Call this to update the image after pretty
  * much any change at all. :-)
@@ -1412,6 +1436,7 @@ if(reset_pos)
  * Without this you get a nasty `bounce' effect whenever scrollbars
  * appear/disappear. But it can still happen when resizing. (XXX)
  */
+adjust_sw_policy();
 do_gtk_stuff();
 
 /* put pixmap onto window as background. We could then free it, but
@@ -1599,8 +1624,7 @@ if(!zoom) return;
 
 listen_to_toggles=0;
 zoom=0;
-gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw_for_pic),
-                               GTK_POLICY_AUTOMATIC,GTK_POLICY_AUTOMATIC);
+adjust_sw_policy();
 xscaling=yscaling=1;
 gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(zoom_widget),zoom);
 listen_to_toggles=1;
@@ -1850,14 +1874,6 @@ if(!listen_to_toggles || in_nextprev) return;
 listen_to_toggles=0;
 
 zoom=!zoom;
-if(zoom)
-  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw_for_pic),
-                               (zoom_panorama&&zoom_panorama_sb)?GTK_POLICY_NEVER:GTK_POLICY_AUTOMATIC,
-                               (zoom_panorama&&!zoom_panorama_sb)?GTK_POLICY_NEVER:GTK_POLICY_AUTOMATIC);
-else
-  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw_for_pic),
-                                 GTK_POLICY_AUTOMATIC,
-                                 GTK_POLICY_AUTOMATIC);
 xscaling=yscaling=1;
 render_pixmap(1);
 
@@ -3720,15 +3736,8 @@ gtk_widget_show(align);
 sw_for_pic=gtk_scrolled_window_new(NULL,NULL);
 GTK_WIDGET_UNSET_FLAGS(sw_for_pic,GTK_CAN_FOCUS);
 gtk_container_set_border_width(GTK_CONTAINER(sw_for_pic),0);
-/* first `POLICY' is horiz, second is vert */
-if(zoom)
-  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw_for_pic),
-                               (zoom_panorama&&zoom_panorama_sb)?GTK_POLICY_NEVER:GTK_POLICY_AUTOMATIC,
-                               (zoom_panorama&&!zoom_panorama_sb)?GTK_POLICY_NEVER:GTK_POLICY_AUTOMATIC);
-else
-  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw_for_pic),
-                                 GTK_POLICY_AUTOMATIC,
-                                 GTK_POLICY_AUTOMATIC);
+/* zoom_panorama_sb hasn't been set yet, but this shouldn't be a problem */
+adjust_sw_policy();
 gtk_paned_add2(GTK_PANED(pane),sw_for_pic);
 gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(sw_for_pic),
                                       align);
