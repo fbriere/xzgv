aa09d9c5a1d368d09c998c1329c850ec4a1c8682
Frédéric Brière <fbriere@fbriere.net>
2026-08-20 23:36:03 -0400

    gtk3: Only call `flist_freeze()` if the list was actually modified
    Freezing a `GtkTreeView` is more disruptive than for a `GtkCList`,
    mostly in that it will break the selection.  We therefore should not
    freeze on operations that only affect the display (i.e. tagging), and
    reserve it for operations that actually modify the file list itself
    (i.e. adding/removing/sorting rows).
    
    (We also freeze in `toggle_thin_rows()`, but that function is kind
    enough to restore the previous selection by itself.)

--- src/main.c
+++ src/main.c
@@ -589,12 +589,8 @@ void cb_tag_all(void)
 {
 int f;
 
-flist_freeze();
-
 for(f=0;f<numrows;f++)
   set_tagged_state(f,1);
-
-flist_thaw();
 }
 
 
@@ -602,12 +598,8 @@ void cb_untag_all(void)
 {
 int f;
 
-flist_freeze();
-
 for(f=0;f<numrows;f++)
   set_tagged_state(f,0);
-
-flist_thaw();
 }
 
 
@@ -615,12 +607,8 @@ void cb_toggle_all(void)
 {
 int f;
 
-flist_freeze();
-
 for(f=0;f<numrows;f++)
   set_tagged_state(f,!get_tagged_state(f));
-
-flist_thaw();
 }
 
 
